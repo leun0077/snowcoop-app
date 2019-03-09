6599186e125a6dc9483108648d64c89653c5ea81
Gabriel Leung <leun0077@algonquinlive.com>
2019-03-09 07:24:02 -0500

    Adding-sideNav-styling

--- src/components/pages/dashboard-page/dashboard-page.component.vue
+++ src/components/pages/dashboard-page/dashboard-page.component.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="dashboardPage">Dashboard page</div>
+  <div id="dashboardPage">
+    <div class="dash">Hi {{firstName}}, welcome to Snowcoop</div>
+  </div>
 </template>
 <script src="./dashboard-page.component.js"></script>
 <style src="./dashboard-page.component.css"></style>
